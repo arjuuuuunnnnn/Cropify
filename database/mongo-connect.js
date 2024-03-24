f3ee257618d08175e1db2f2aa85fcb5635e597c9
Atheek Hebbar <119834221+atheek2003@users.noreply.github.com>
2024-03-24 10:41:35 +0530

    database

---
+++ database/mongo-connect.js
@@ -0,0 +1,26 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://atheekhebbar46:UalU4k7cmlIV4yjh@cluster0.e03ugkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  catch{
+    console.log("Unable to Connect")
+  }
+}
+run().catch(console.dir);
